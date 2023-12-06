d6fd2dd2fea58e27eddf46fff4c53a622ef98fa7
Rangsan2001 <rangsan6095@gmail.com>
2023-12-06 11:39:45 +0700

    run project with docker-compose

---
+++ application/DemoApp/DemoApp/AuthenticationAPI/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using AuthenticationModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            return Ok(request);
+        }
+    }
+}
